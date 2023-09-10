6bb30e42a646f07cf7ff4cd01ae3b2732562caec
Joorg99 <jorgegallero8@gmail.com>
2023-09-10 16:23:44 -0300

    primer commit: capa de datos creacion de modelo tabla libros

---
+++ Trabajoprog3/Trabajoprog3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Trabajoprog3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Trabajoprog3")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("107173c5-3e12-4285-ab5a-78d809d82683")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ bdd/bdd.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE libreria;
+USE libreria;
+CREATE TABLE libros (
+id INT(8) AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(20),
+icbn INT(15) UNIQUE NOT NULL,
+autor VARCHAR(30),
+genero VARCHAR(10),
+fecha_lanzamiento VARCHAR(60),
+cantidad_paginas INT(4)
+);
+
+CREATE TABLE socios(
+id int auto_increment primary key,
+apellido varchar(15),
+nombre varchar(15),
+telefono INT(12)
+);
+
+CREATE TABLE prestamos (
+id INT AUTO_INCREMENT PRIMARY KEY,
+libro_id INT(8),
+socio_id INT(8),
+fecha_prestamo varchar(20),
+fecha_devolucion VARCHAR(20),
+FOREIGN KEY (libro_id) REFERENCES libros(id),
+FOREIGN KEY (socio_id) REFERENCES socios(id)
+);
